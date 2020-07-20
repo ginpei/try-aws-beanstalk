c83f5a4d7c82a7ce0f51abe926f227dae4b1bab7
ginpei <ginpei@gmail.com>
2020-07-20 01:23:20 -0700

    update to show port

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(`Hello World from ${port}!`);
 });
 
 app.listen(port, () => process.stdout.write(`Listening at ${port}\n`));
